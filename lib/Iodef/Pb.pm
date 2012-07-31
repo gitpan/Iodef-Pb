6c37f6d21dac93baca817238b94d75937e1f9d03
Wes Young <saxjazman@cpan.org>
2012-07-31 14:58:14 +0000

    Import of SAXJAZMAN/Iodef-Pb-0.01_02 from CPAN.
    gitpan-cpan-distribution: Iodef-Pb
    gitpan-cpan-version:      0.01_02
    gitpan-cpan-path:         SAXJAZMAN/iodef/Iodef-Pb-0.01_02.tar.gz
    gitpan-cpan-author:       SAXJAZMAN
    gitpan-cpan-maturity:     developer

--- lib/Iodef/Pb.pm
+++ lib/Iodef/Pb.pm
@@ -6,17 +6,108 @@ use 5.008008;
 use strict;
 use warnings;
 
-our $VERSION = '0.01';
+our $VERSION = '0.01_02';
 $VERSION = eval $VERSION;  # see L<perlmodstyle>
+
+require Exporter;
+
+our @ISA = qw(Exporter);
+
+# Items to export into callers namespace by default. Note: do not export
+# names by default without a very good reason. Use EXPORT_OK instead.
+# Do not simply export all your public functions/methods/constants.
+
+# This allows declaration   use Iodef::Pb ':all';
+# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
+# will save memory.
+our %EXPORT_TAGS = ( 'all' => [ qw(
+    iodef_descriptions iodef_assessments iodef_confidence iodef_impacts iodef_impacts_first
+    iodef_additional_data iodef_systems iodef_addresses iodef_events_additional_data iodef_services
+    iodef_systems_additional_data
+) ] );
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+our @EXPORT = qw//;
 
 # most methods generated in Iodef::_Pb
 
-sub iodef_addresses {
-    my $class = shift;
+sub iodef_descriptions {
     my $iodef = shift;
     
     my @array;
     foreach my $i (@{$iodef->get_Incident()}){
+        my $desc = $i->get_Description();
+        $desc = [$desc] unless(ref($desc) eq 'ARRAY');
+        push(@array,@$desc);
+    }
+    return(\@array);
+}
+
+sub iodef_confidence {
+    my $iodef = shift;
+    
+    my $ret = iodef_assessments($iodef);
+    my @array;
+    foreach my $a (@$ret){
+        push(@array,$a->get_Confidence());
+    }
+    return(\@array);
+}
+
+sub iodef_assessments {
+    my $iodef = shift;
+    
+    return [] unless(ref($iodef) eq 'IODEFDocumentType');
+
+    my @array;
+    foreach my $i (@{$iodef->get_Incident()}){
+        unless(ref($i) eq 'IncidentType'){
+            warn 'invalid type: '.ref($i) if($::debug);
+            return [];
+        }
+        push (@array,@{$i->get_Assessment()});
+    }
+    return(\@array);
+}
+
+sub iodef_impacts {
+    my $iodef = shift;
+    
+    return [] unless(ref($iodef) eq 'IODEFDocumentType');
+    
+    my $assessments = iodef_assessments($iodef);
+    my @array;
+    
+    foreach my $a (@$assessments){
+        unless(ref($a) eq 'AssessmentType'){
+            warn 'invalid type: '.ref($a) if($::debug);
+            return [];
+        }
+        push(@array,@{$a->get_Impact()});
+    }
+    return(\@array);
+}
+
+sub iodef_impacts_first {
+    my $iodef = shift;
+
+    return [] unless(ref($iodef) eq 'IODEFDocumentType');
+
+    my $impacts = iodef_impacts($iodef);
+    my $impact = @{$impacts}[0];
+    return($impact);
+}
+
+sub iodef_addresses {
+    my $iodef = shift;
+    my $type = shift;
+    
+    $type = iodef_address_type($type) if($type);
+    
+    return [] unless(ref($iodef) eq 'IODEFDocumentType');
+        
+    my @array;
+    foreach my $i (@{$iodef->get_Incident()}){
+        next unless($i->get_EventData());
         foreach my $e (@{$i->get_EventData()}){
             my @flows = (ref($e->get_Flow()) eq 'ARRAY') ? @{$e->get_Flow()} : $e->get_Flow();
             foreach my $f (@flows){
@@ -33,6 +124,118 @@ sub iodef_addresses {
         }
     }
     return(\@array);
+}
+
+sub iodef_services {
+    my $iodef = shift;
+    
+    my @array;
+    foreach my $i (@{$iodef->get_Incident()}){
+        next unless($i->get_EventData());
+        foreach my $e (@{$i->get_EventData()}){
+            my @flows = (ref($e->get_Flow()) eq 'ARRAY') ? @{$e->get_Flow()} : $e->get_Flow();
+            foreach my $f (@flows){
+                my @systems = (ref($f->get_System()) eq 'ARRAY') ? @{$f->get_System()} : $f->get_System();
+                foreach my $s (@systems){
+                    my $services = $s->get_Service();
+                    $services = [$services] unless(ref($services) eq 'ARRAY');
+                    foreach my $svc (@$services){
+                        $svc = [$svc] unless(ref($svc) eq 'ARRAY');
+                        push(@array,@$svc);
+                    }
+                }
+            }
+        }
+    }
+    return(\@array);
+}
+
+sub iodef_systems {
+    my $iodef = shift;
+    
+    my @array;
+    foreach my $i (@{$iodef->get_Incident()}){
+        next unless($i->get_EventData());
+        foreach my $e (@{$i->get_EventData()}){
+            my @flows = (ref($e->get_Flow()) eq 'ARRAY') ? @{$e->get_Flow()} : $e->get_Flow();
+            foreach my $f (@flows){
+                my @systems = (ref($f->get_System()) eq 'ARRAY') ? @{$f->get_System()} : $f->get_System();
+                push(@array,@systems);
+            }
+        }
+    }
+    return(\@array);
+}
+
+sub _additional_data {
+    my $array = shift;
+    
+    my $return_array;
+    foreach my $e (@$array){
+        next unless($e->get_AdditionalData());
+        my @additional_data = (ref($e->get_AdditionalData()) eq 'ARRAY') ? @{$e->get_AdditionalData()} : $e->get_AdditionalData();
+        push(@$return_array,@additional_data);
+    }
+    return($return_array);
+}     
+
+sub iodef_additional_data {
+    my $iodef = shift;
+    
+    return [] unless(ref($iodef) eq 'IODEFDocumentType');
+    #my @array;
+    #foreach my $i (@{$iodef->get_Incident()}){
+    #    next unless($i->get_AdditionalData());
+    #    my @additional_data = (ref($i->get_AdditionalData()) eq 'ARRAY') ? @{$i->get_AdditionalData()} : $i->get_AdditionalData();
+    #    push(@array,@additional_data);
+    #}
+    
+    my $array = _additional_data($iodef->get_Incident());
+    return($array);
+}
+
+sub iodef_events_additional_data {
+    my $iodef = shift;
+    
+    my $array = [];
+    foreach my $i (@{$iodef->get_Incident()}){
+        next unless($i->get_EventData());
+        if(my $ret = _additional_data($i->get_EventData())){
+            push(@$array,@$ret);
+        }
+        #foreach my $e (@{$i->get_EventData()}){
+        #    my @additional_data = (ref($e->get_AdditionalData()) eq 'ARRAY') ? @{e->get_AdditionalData()} : $e->get_AdditionalData();
+        #    push(@array,@additional_data);
+        #}
+    }
+    
+    return($array);
+}
+
+sub iodef_systems_additional_data {
+    my $iodef = shift;
+    
+    my $array;
+    foreach my $i (@{$iodef->get_Incident()}){
+        next unless($i->get_EventData());
+        foreach my $e (@{$i->get_EventData()}){
+            my @flows = (ref($e->get_Flow()) eq 'ARRAY') ? @{$e->get_Flow()} : $e->get_Flow();
+            foreach my $f (@flows){
+                next unless($f->get_System());
+                my @systems = (ref($f->get_System()) eq 'ARRAY') ? @{$f->get_System()} : $f->get_System();
+                if(my $ret = _additional_data($f->get_System())){
+                    push(@$array,@$ret);
+                }
+                #foreach my $s (@systems){
+                #    next unless($s->get_AdditionalData());
+                #    my @additional_data = (ref($s->get_AdditionalData()) eq 'ARRAY') ? @{$s->get_AdditionalData()} : $s->get_AdditionalData();
+                #    next unless($#additional_data > -1);
+                #    push(@array,@additional_data);
+                #}
+            }
+        }
+    }
+    return($array);
 }
 
 1;
